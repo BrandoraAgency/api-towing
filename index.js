45919ef294bb37404fce3c9e03905f355f089503
masab-m <masabm263@gmail.com>
2023-01-27 18:38:23 +0500

    db update

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ connectDatabase()
 app.get('/api', (req, res) => {
   res.json({message:'Hello World!'})
 })
-// db.sequelize.sync().then((req)=>{
+db.sequelize.sync().then((req)=>{
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
-// })
+})
