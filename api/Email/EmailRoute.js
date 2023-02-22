7f00b2949b2080e131e11142d36c61ce8a924749
masab-m <masabm263@gmail.com>
2023-02-22 21:54:50 +0500

    update

--- api/Email/EmailRoute.js
+++ api/Email/EmailRoute.js
@@ -1,7 +1,4 @@
 const { nodeMail, sendmail } = require("./Email")
-const multer = require('multer');
-// configure multer to handle file uploads
-const upload = multer({ dest: 'temp/' });
 
 module.exports=(app)=>{
     app.post('/api/email' ,nodeMail,sendmail)
